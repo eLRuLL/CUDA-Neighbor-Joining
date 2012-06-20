bf1ba3f3d8ae1aafe09b0290628f92cb435ecc7b
Raul Omar Gallegos Hidalgo <raul.ogh@gmail.com>
2012-06-20 02:33:11 -0500

    Step2 Kernel fixed, host step2 launch
    The kernel for step2 was finished but not tested yet. The host code to
    launch the step2 kernel is also completed but not tested.
    There is also comments that have to be completed with code of the rest
    of the steps.

--- nj.cu
+++ nj.cu
@@ -7,13 +7,12 @@
 
 using namespace std;
 
-__global__ void nj_step1(float* mat, float* res,int width)
+__global__ void nj_step1(float* mat, float* res,int width)		// Calculate the tree-divergence for every object.
 {
 	size_t idx = blockIdx.x * blockDim.x + threadIdx.x;
 	if(idx < width){
 		float rpta = 0.0f;
-		for(int i=0; i<width; i++)
-		{
+		for(int i=0; i<width; i++){
 			if(i<idx)
 				rpta += mat[idx*width + i];
 			else
@@ -23,24 +22,29 @@ __global__ void nj_step1(float* mat, float* res,int width)
 	}
 }
 
-__global__ void nj_step2(float* mat, float* mat2, float* diverg, int width, int* fronteras)
+__global__ void nj_step2(float* mat_t, float* mat, float* diverg, int width, int* limits) // Calculate a new matrix (Mt) of distances.
 {
 	int bx = blockIdx.x;
 	
-	int i = 0;
-	int currfil = 0;
-	int currcol = 0;
-	while(fronteras)
-	{
-		if(bx < fronteras[i])
-			currfil = 1;
-			currcol = fronteras[i-1];
+	int k = 0;
+	int blockfil = 0;
+	int blockcol = 0;
+	while(limits[k] && limits[k] < bx){
+		k++;
 	}
+	if(k!=0)
+		blockfil = k - 1;
 	
-	int idx = blockIdx.x + blockDim.x + threadIdx.x;
-	int idy = blockIdx.y + blockDim.y + threadIdx.y;
+	if(k!=0)
+		blockcol = limits[k - 1];
 	
-	mat2[idx*width + idy] = mat[idx*width + idy] - (diverg[idx] + diverg[idy])/(width-2);
+	int idx = threadIdx.x;
+	int idy = threadIdx.y;
+	
+	int i = (blockfil * blockDim.x) + idx;
+	int j = (blockcol * blockDim.y) + idy;
+	
+	mat_t[i*width + j] = mat[i*width + j] - (diverg[i] + diverg[j])/(width-2);
 }
 
 int main()
@@ -109,7 +113,7 @@ int main()
 		cudaMalloc((void**) &M_d, sizeof(float)*N*N);
 		cudaMemcpy(M_d,M,sizeof(float)*N*N,cudaMemcpyHostToDevice);
 		
-		nj_step1<<<numblocks,N>>>(M_d,r_d,N);			// Kernel launch.
+		nj_step1<<<numblocks,N>>>(M_d,r_d,N);			// Kernel launch for step 1.
 		
 		cudaMemcpy(r,r_d,sizeof(float)*N,cudaMemcpyDeviceToHost);	// Copying response array to the Host.
 		
@@ -117,16 +121,34 @@ int main()
 		for(int i=0; i<N; i++)
 			printf("%4.2f ",r[i]);
 		printf("\n");
-		//int numblocks = ((N/b)*((N/b)+1))/2.0;
-		//float* matrix_temp;
 		
-		//int* fronteras = new int[N/b];
-		//for(int i=0; i<N/b ; i++)
-		//{
-	//		fronteras[i] = ((i+1)*(i+2))/2.0;
-	//	}
+		int numblocks = ((N/b)*((N/b)+1))/2.0;			// Number of blocks like a triangular matrix.
 		
-		//nj_step2<<<numblocks,(b,b)>>>(matrix,matrix_temp,divergencias,width,fronteras);
+		int* limits = new int[N/b];
+		for(int i=0; i<N/b ; i++)
+			limits[i] = (int)((((i+1)*(i+2))/2.0) - 1);
+		
+		float* Mt_d;
+		cudaMalloc((void**) &Mt_d, sizeof(float)*N*N);
+		float* limits_d;
+		cudaMalloc((void**) &limits_d, sizeof(float)*N/b);
+		
+		nj_step2<<<numblocks,(b,b)>>>(Mt_d,M_d,r_d,N,limits_d);	// Kernel launch for step 2.
+		
+		cudaMemcpy(Mt,Mt_d,sizeof(float)*N*N,cudaMemcpyDeviceToHost);	// Copying response matrix to the Host.
+		
+		// Printing temporal distance matrix (Mt).
+		for(int i=0; i<N; i++){
+			for(int j=0; j<N; j++)
+				printf("%4.2f ",Mt[i*N + j]);
+			printf("\n");
+		}
+		
+		// Step 3: Select objects "i" and "j" where M[i][j] is the minimum,
+		// Step 4: Create a new object U and delete "i" and "j".
+		// Step 5: Calculate distances from "i" to U and "j" to U.
+		// Step 6: Calculate the distance between U and the rest.
+		
 		N = N - 1;
 	}
 	return 0;
